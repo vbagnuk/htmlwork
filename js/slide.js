abfc8155c7b4e1d3e235cd7710de32e31b616e71
Vadim <vbagnuk86@gmail.com>
2017-04-17 18:08:39 +0300

    1234

---
+++ js/slide.js
@@ -0,0 +1,13 @@
+if(!/iPhone/i.test(navigator.userAgent)) {
+    $('.active > div > video').get(0).play();
+
+    $('#carousel').bind('slide.bs.carousel', function(e) {
+      $(e.relatedTarget).find('video').get(0).play();
+    });
+
+    $('#carousel').bind('slid.bs.carousel', function(e) {
+      $('video').not('.active > div > video').each(function() {
+        $(this).get(0).pause();
+      });
+    });
+  }
